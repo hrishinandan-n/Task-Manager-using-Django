e12275497752e96497c7b82dcace1b98e80b6f40
HRISHI-26 <hrishiwynd26@gmail.com>
2025-11-10 21:20:36 +0530

    Added Root URL

--- TaskManager/urls.py
+++ TaskManager/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView ,SpectacularSwaggerView
+
+
+def home(request):
+    return HttpResponse("Welcome to Task Manager")
 
 # Root URL configuration for TaskManager project
 urlpatterns = [
@@ -9,11 +14,17 @@ urlpatterns = [
     # App routes
     path('tasks/', include('tasks.urls')),  # Task management
     path('users/', include('users.urls')),  # User authentication and registration
+    path('', home), # Root URL
 
     # API routes
+
     path('api/v1/', include('api.urls')),  # Versioned API endpoints
+
+    # path('api/v1', include('api.urls')),  # Versioned API endpoints
+
 
     # API Schema & Interactive Docs
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),  # Generates OpenAPI schema (YAML/JSON)
     path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema')),  # Swagger UI
 ]
+
